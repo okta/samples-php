fd5c924f890737d8f27b90170d8ebdf3ec6f638f
Brian Retterer <bretterer@gmail.com>
2018-02-21 17:04:38 -0800

    Move env to root of project

--- custom-login/index.php
+++ custom-login/index.php
@@ -6,7 +6,7 @@ require_once __DIR__ . '/functions.php';
 $loader = new Twig_Loader_Filesystem(__DIR__ . '/views');
 $twig = new Twig_Environment($loader);
 
-$dotenv = new Dotenv\Dotenv(__DIR__);
+$dotenv = new Dotenv\Dotenv(dirname(__DIR__));
 $dotenv->load();
 
 $state = 'applicationState';
